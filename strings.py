f6e626262877bdd63cf27e669985649c198cae3b
Emilio Araos <ea3dev@gmail.com>
2020-01-09 16:37:38 -0700

    reversing string

--- strings.py
+++ strings.py
@@ -8,3 +8,11 @@ my_string1 = 'abcdefghijk'
 print(my_string1)
 print (my_string1[2:])
 print(my_string1[:3])
+
+print(my_string1[3:6])
+print(my_string1[3:9]) 
+print(my_string1[::])     # prints the string from start to finish.
+print(my_string1[::2])   # jump in step size # 2.
+print(my_string1[2:7:2])   # prints index 2, up to but not including index 7, in steps of 2. 
+print(my_string1[::-1])  # reverses the string. 
+ 
