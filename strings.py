038d6f1e6ba87d14f1e95575ab384c6d9d84e125
Emilio Araos <ea3dev@gmail.com>
2020-01-09 16:57:22 -0700

    string concatenation

--- strings.py
+++ strings.py
@@ -15,4 +15,23 @@ print(my_string1[::])     # prints the string from start to finish.
 print(my_string1[::2])   # jump in step size # 2.
 print(my_string1[2:7:2])   # prints index 2, up to but not including index 7, in steps of 2. 
 print(my_string1[::-1])  # reverses the string. 
+
+
+#immutability of strings. 
+
+name = 'Sam'
+
+# name [0] = 'P'   Won't work
+
+last_letters = name [1:]
+
+print(last_letters)
+
+print('P' + last_letters)  # String concatenation. 
+
+x = 'Hello World'
+print(x + " it is beautiful outside")
+
+
+
  
